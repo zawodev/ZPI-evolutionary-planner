fe09aa282b9add96e72c0deacaeb41fcab6b79a4
zawodev <alikstepaniuk@gmail.com>
2025-10-20 13:11:51 +0200

    GET/PUT for preferences
    +other fixes

--- backend/identity/admin.py
+++ backend/identity/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Organization, Group, UserGroup
 
-# Register your models here.
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('role',)}),
+    )
+
+
 admin.site.register(Organization)
 admin.site.register(Group)
 admin.site.register(UserGroup)
--- backend/preferences/urls.py
+++ backend/preferences/urls.py
@@ -3,17 +3,17 @@ from . import views
 
 urlpatterns = [
     path(
-        'user-preferences/<uuid:recruitment_id>/<int:user_id>/',
+        'user-preferences/<str:recruitment_id>/<str:user_id>/',
         views.user_preferences_view,
         name='user-preferences'
     ),
     path(
-        'constraints/<uuid:recruitment_id>/',
+        'constraints/<str:recruitment_id>/',
         views.constraints_view,
         name='constraints'
     ),
     path(
-        'management-preferences/<uuid:recruitment_id>/',
+        'management-preferences/<str:recruitment_id>/',
         views.management_preferences_view,
         name='management-preferences'
     ),
